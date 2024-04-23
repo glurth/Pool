9cf4b0cc3700ca80289939cf54771dbd371a3ec8
Glurth <Glurth@gmail.com>
2024-04-23 09:23:34 -0600

    first commit

---
+++ Runtime/MonoPool.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using System;
+namespace EyE.Collections
+{
+    /// <summary>
+    /// This variant of Pool<typeparamref name="T"/> works on MonoBehaviour derived classes.  
+    /// When new elements are created for the pool, it will use the defaultPoolValue as a preFabTemplace to instantiate a new version (as a child of the instantiationParent member).
+    /// The option to enable and disable the pool element, when Pulled or Tossed is provided via the enableOnPullDisenableOnToss member.
+    /// Note: this class does not affect the active-state of the GameObject.
+    /// </summary>
+    /// <typeparam name="T">A type of component added to unity GameObjects</typeparam>
+    public class MonoPool<T> : Pool<T> where T : MonoBehaviour
+    {
+        public bool enableOnPullDisenableOnToss = true;
+        public Transform instantiationParent;
+
+        /// <summary>
+        /// populates the parameter with a new instance of a <typeparamref name="T"/>
+        /// </summary>
+        /// <param name="value">the calling function will have the param filled.</param>
+        protected override void AssignDefaultValueTo(out T value)
+        {
+            if (defaultPoolValue == null)
+                throw new System.NullReferenceException(GetType().Name + " has not been assigned a defaultPoolValue, and so cannot create new ones based on it.");
+            value = Component.Instantiate<T>(defaultPoolValue, instantiationParent);
+        }
+
+        void OnMonoPull(T pulledObject)
+        {
+            if (enableOnPullDisenableOnToss)
+                pulledObject.enabled=true;
+        }
+        void OnMonoToss(T objectPutBackInPool)
+        {
+            if (enableOnPullDisenableOnToss)
+                objectPutBackInPool.enabled = false;
+        }
+
+        /// <summary>
+        /// It is recommended that you use the 2(or more) parameter versions of MonoPool constructors to properly initialize for Unity.
+        /// A warning will be generated in the console if this version is used, but it will otherwise try to proceed as normal.
+        /// </summary>
+        public MonoPool() : base()
+        {
+            Debug.LogWarning("You are using the paramaterless constructor on a MonoPool. The defaultPoolValue will remain null.\nUse the MonoPool(T defaultPoolValue, Transform instantiationParent) version instead.\nIf you manually set these value later, this warning can be ignored");
+            this.onPull = (a) => { OnMonoPull(a); };
+            this.onToss = (a) => { OnMonoToss(a); };
+        }
+
+        /// <summary>
+        /// Used to create a new MonoPool object.  It takes parameters that specify how new elements, that don't exist yet, should be created.
+        /// </summary>
+        /// <param name="defaultPoolValue">This component reference will be used as a "preFab" to Instantiate new pool elements into the scene.</param>
+        /// <param name="instantiationParent">A Transform that specifies the parent of any newly created elements.</param>
+        public MonoPool(T defaultPoolValue, Transform instantiationParent) : base(defaultPoolValue)
+        {
+            this.instantiationParent = instantiationParent;
+            this.onPull = (a) => { OnMonoPull(a); };
+            this.onToss = (a) => { OnMonoToss(a); };
+        }
+
+
+        /// <summary>
+        /// Used to create a new MonoPool object.  It takes parameters that specify how new elements, that don't exist yet, should be created.
+        /// Provides parameters so actions can be added 
+        /// </summary>
+        /// <param name="defaultPoolValue">This component reference will be used as a "preFab" to Instantiate new pool elements into the scene.</param>
+        /// <param name="instantiationParent">A Transform that specifies the parent of any newly created elements.</param>
+        /// <param name="onPull">Action that will be invoked when an object is pulled from the pool.</param>
+        /// <param name="onToss">Action that will be invoked when an object is tossed into the pool.</param>
+        public MonoPool(T defaultPoolValue, Transform instantiationParent, Action<T> onPull, Action<T> onToss) : base(defaultPoolValue)
+        {
+            this.instantiationParent = instantiationParent;
+            this.onPull = (a) => { OnMonoPull(a); onPull(a); };
+            this.onToss = (a) => { OnMonoToss(a); onToss(a); };
+        }
+    }
+
+
+    /// <summary>
+    /// This variant of Pool<typeparamref name="T"/> works on unity GameObjects.  
+    /// When new elements are created for the pool, it will use the defaultPoolValue as a preFabTemplace to instantiate a new version (as a child of the instantiationParent member).
+    /// The option to Activate and Deactivate the pool element's GameObject, when Pulled or Tossed is provided via the SetActiveOnPullDeactiveOnToss member.
+    /// Callback functions, invoked when objects are pulled or tossed from the pool can, optionally, be passed in during construction.
+    /// </summary>
+    /// <typeparam name="T">A type of component added to unity GameObjects</typeparam>
+    public class GameObjectPool : Pool<GameObject>
+    {
+        public bool SetActiveOnPullDeactiveOnToss = true;
+        public Transform instantiationParent;
+
+        /// <summary>
+        /// populates the parameter with a new instance of a <typeparamref name="T"/>
+        /// </summary>
+        /// <param name="value">the calling function will have the param filled.</param>
+        protected override void AssignDefaultValueTo(out GameObject value)
+        {
+            if (defaultPoolValue == null)
+                throw new System.NullReferenceException(GetType().Name + " has not been assigned a defaultPoolValue, and so cannot create new ones based on it.");
+            value = Component.Instantiate<GameObject>(defaultPoolValue, instantiationParent);
+        }
+
+        void OnMonoPull(GameObject pulledObject)
+        {
+            if (SetActiveOnPullDeactiveOnToss)
+                pulledObject.SetActive(true);
+        }
+        void OnMonoToss(GameObject objectPutBackInPool)
+        {
+            if (SetActiveOnPullDeactiveOnToss)
+                objectPutBackInPool.SetActive(false);
+        }
+
+        /// <summary>
+        /// It is recommended that you use the 2(or more) parameter versions of MonoPool constructors to properly initialize for Unity.
+        /// A warning will be generated in the console if this version is used, but it will otherwise try to proceed as normal.
+        /// </summary>
+        public GameObjectPool() : base()
+        {
+            Debug.LogWarning("You are using the paramaterless constructor on a MonoPool. The defaultPoolValue will remain null.\nUse the MonoPool(T defaultPoolValue, Transform instantiationParent) version instead.\nIf you manually set these value later, this warning can be ignored");
+            this.onPull = (a) => { OnMonoPull(a); };
+            this.onToss = (a) => { OnMonoToss(a); };
+        }
+
+        /// <summary>
+        /// Used to create a new MonoPool object.  It takes parameters that specify how new elements, that don't exist yet, should be created.
+        /// </summary>
+        /// <param name="defaultPoolValue">This component reference will be used as a "preFab" to Instantiate new pool elements into the scene.</param>
+        /// <param name="instantiationParent">A Transform that specifies the parent of any newly created elements.</param>
+        public GameObjectPool(GameObject defaultPoolValue, Transform instantiationParent) : base(defaultPoolValue)
+        {
+            this.instantiationParent = instantiationParent;
+            this.onPull = (a) => { OnMonoPull(a); };
+            this.onToss = (a) => { OnMonoToss(a); };
+        }
+
+
+        /// <summary>
+        /// Used to create a new MonoPool object.  It takes parameters that specify how new elements, that don't exist yet, should be created.
+        /// Provides parameters so actions can be added 
+        /// </summary>
+        /// <param name="defaultPoolValue">This component reference will be used as a "preFab" to Instantiate new pool elements into the scene.</param>
+        /// <param name="instantiationParent">A Transform that specifies the parent of any newly created elements.</param>
+        /// <param name="onPull">Action that will be invoked when an object is pulled from the pool. It will be passed the object that was just pulled.</param>
+        /// <param name="onToss">Action that will be invoked when an object is tossed into the pool. It will be passed the object that was just tossed.</param>
+        public GameObjectPool(GameObject defaultPoolValue, Transform instantiationParent, Action<GameObject> onPull, Action<GameObject> onToss) : base(defaultPoolValue) //note we don't call the base version that takes action params- we'll create our own here.
+        {
+            this.instantiationParent = instantiationParent;
+            this.onPull = (a) => { OnMonoPull(a); onPull(a); };
+            this.onToss = (a) => { OnMonoToss(a); onToss(a); };
+        }
+    }
+}
---
+++ Runtime/Pool.cs
@@ -0,0 +1,165 @@
+using System.Collections.Generic;
+using System;
+
+namespace EyE.Collections
+{
+    /// <summary>
+    ///This is an implementation of a generic object pool. An object pool is a container that holds a collection of objects that can be reused, rather than creating a new instance each time an object is needed. This can be useful for improving performance and reducing memory usage, as it can be more efficient to reuse objects rather than creating new ones.
+    ///The class has several methods for adding objects back to the pool (Toss), and retrieving objects from the pool (Pull). It also has two delegate properties onPull and onToss, which are called each time an object is retrieved from or added back to the pool. The class can be initialized with a default value for objects in the pool, and this default value will be used if the pool is empty when an object is requested.
+    ///The class implements a list to store the objects in the pool, and it uses the RemoveAt method to retrieve the last item in the list each time an object is requested, ensuring that the most recently added object is used first.
+    ///Overall, this implementation provides a basic object pool implementation that can be customized by defining custom actions for onPull and onToss and by providing a default value for objects in the pool.
+    /// 
+    /// </summary>
+    /// <typeparam name="T">The type of object stored in the pool.</typeparam>
+    public class Pool<T>
+    {
+        /// <summary>
+        /// The default value of new items pulled from the pool.
+        /// </summary>
+        protected T defaultPoolValue;
+        /// <summary>
+        /// The action to be performed when an item is pulled from the pool.
+        /// </summary>
+        protected Action<T> onPull = (T) => { };
+
+        /// <summary>
+        /// The action to be performed when an item is tossed back into the pool.
+        /// </summary>
+        protected Action<T> onToss = (T) => { };
+
+        /// <summary>
+        /// The list of contents in the pool.
+        /// </summary>
+        public List<T> poolConents = new List<T>();
+
+        /// <summary>
+        /// The pool will keep a record of all items that have been pulled from it.  Invoking TossAll() will toss them all back into the pool.
+        /// Keeping this option true will increase the time required to perform pull and toss operations.
+        /// Setting this option to true after pool operation starts is not recommended.
+        /// </summary>
+        bool recordPulled = true;
+
+        List<T> pulledList = new List<T>();
+        /*
+        /// <summary>
+        /// Constructor that sets the default pool value to the default value of T.
+        /// </summary>
+        public Pool() { defaultPoolValue = default(T); }
+        */
+        /// <summary>
+        /// Constructor the specifies if pull objects should be recorded
+        /// </summary>
+        /// <param name="recordPulled">should pulled objects be recorded? (required for TossAllBack() )</param>
+        public Pool(bool recordPulled = true) { defaultPoolValue = default(T); this.recordPulled = recordPulled; }
+
+        /// <summary>
+        /// Sets the default pool value.
+        /// </summary>
+        /// <param name="defaultPoolValue">The new default pool value</param>
+        public void SetDefault(T defaultPoolValue) { this.defaultPoolValue = defaultPoolValue; }
+
+
+
+        /// <summary>
+        /// Constructor that sets the default pool value.
+        /// </summary>
+        /// <param name="defaultPoolValue">The default pool value</param>
+        /// <param name="recordPulled">should pulled objects be recorded? (required for TossAllBack() )</param>
+        public Pool(T defaultPoolValue, bool recordPulled=true) { this.defaultPoolValue = defaultPoolValue; this.recordPulled = recordPulled; }
+
+        /// <summary>
+        /// Constructor that sets the default pool value, the on pull action and the on toss action.
+        /// </summary>
+        /// <param name="defaultPoolValue">The default pool value</param>
+        /// <param name="onPull">The action to be performed when an item is pulled from the pool</param>
+        /// <param name="onToss">The action to be performed when an item is tossed back into the pool</param>
+        /// <param name="recordPulled">should pulled objects be recorded? (required for TossAllBack() )</param>
+        public Pool(T defaultPoolValue, Action<T> onPull, Action<T> onToss, bool recordPulled=true)
+        {
+            this.defaultPoolValue = defaultPoolValue;
+            this.recordPulled = recordPulled;
+            this.onPull = onPull;
+            this.onToss = onToss;
+        }
+
+        /// <summary>
+        /// Tosses an item back into the pool.
+        /// </summary>
+        /// <param name="valueToPutBackInPool">The item to be tossed back into the pool</param>
+        public void Toss(T valueToPutBackInPool)
+        {
+            poolConents.Add(valueToPutBackInPool);
+            if (recordPulled)
+                pulledList.Remove(valueToPutBackInPool);
+            onToss(valueToPutBackInPool);
+        }
+
+        /// <summary>
+        /// Tosses a list of items back into the pool.
+        /// </summary>
+        /// <param name="valueToPutBackInPool">The list of items to be tossed back into the pool</param>
+        public void Toss(IList<T> valueToPutBackInPool)
+        {
+            foreach (T element in valueToPutBackInPool)
+            {
+                Toss(element);
+            }
+        }
+
+
+        /// <summary>
+        /// Pulls an item from the pool.
+        /// </summary>
+        /// <returns>The item pulled from the pool</returns>
+        public T Pull()
+        {
+            T returnValue;
+            if (poolConents.Count != 0)
+            {
+                returnValue = poolConents[poolConents.Count - 1];
+                poolConents.RemoveAt(poolConents.Count - 1);
+            }
+            else
+            {
+                AssignDefaultValueTo(out returnValue);
+            }
+            onPull(returnValue);
+            if (recordPulled)
+                pulledList.Add(returnValue);
+            return returnValue;
+        }
+
+        /// <summary>
+        /// This function can be overridden in descendants to specify how new objects that don't yet exist in the pool should be initialized.
+        /// </summary>
+        /// <param name="value">this object will be filled with, or set to a reference to, new objects when they are pulled from the pool</param>
+        protected virtual void AssignDefaultValueTo(out T value) { value = defaultPoolValue; }
+
+        /// <summary>
+        /// Function used to pull a number of items from the pool
+        /// </summary>
+        /// <param name="count">number of items to pull from the poll</param>
+        /// <param name="addTo">a list that these pulled items will be appended to</param>
+        public void Pull(int count, IList<T> addTo)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                addTo.Add(Pull());
+            }
+        }
+
+        /// <summary>
+        /// This function will only work if recordPulled is enabled.  Will throw an expection if invoked without it. 
+        /// </summary>
+        public void TossAllBack()
+        {
+            if (!recordPulled) throw new System.ArgumentException("TossAllBack has been invoked on a Pool, but this Pool has the recordPull member set to false.");
+            foreach (T element in pulledList)
+            {
+                Toss(element);
+            }
+            pulledList.Clear();
+        }
+    }
+}
+
